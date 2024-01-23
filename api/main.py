f037f39c4749e0c83d5d7958ee1fff424b201005
N.Minayev <minyaev@jivosite.com>
2024-01-24 01:37:33 +0400

    ref

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from repository.parts_repository import PartRepository
+import sys
+
 
 if __name__ == '__main__':
     repo = PartRepository()
-    # r = repo.create_part("ramka")
-    # r = repo.get_part("ramka")
+    # r = repo.create_part(sys.argv[1])
+    # r = repo.get_part(sys.argv[1])
     r = repo.get_all_parts()
     print(r)
